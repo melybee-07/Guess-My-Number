e2fb41854eec9c40bbd8ffcd1ffa1d3835b79226
MELISSA MAZIR <melissamazir07@gmail.com>
2023-04-17 18:08:40 +0100

    CHANging the code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let choosenNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(choosenNumber);
+
 const lukyNumber = document.querySelector('.number');
 
 let confettiSettings = { target: 'confetti-canvas' };
@@ -12,7 +12,6 @@ let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
@@ -31,23 +30,10 @@ document.querySelector('.check').addEventListener('click', function () {
 
     const audio = new Audio('./children-yay-sound-effectmp3-320k_VB7Ch4TH.mp3');
     audio.play();
-  } else if (guess < choosenNumber) {
+  } else if (guess !== choosenNumber) {
     if (Score > 1) {
-      document.querySelector('.message').textContent = '📉 too low!';
-      Score--;
-      document.querySelector('.score').textContent = Score;
-    } else {
       document.querySelector('.message').textContent =
-        '💥 you have lost the game';
-      document.querySelector('.score').textContent = 0;
-      const audioLoose = new Audio(
-        './Arcade game over sound effect_(MP3_320K).mp3'
-      );
-      audioLoose.play();
-    }
-  } else if (guess > choosenNumber) {
-    if (Score > 1) {
-      document.querySelector('.message').textContent = '📈 too hight!';
+        guess > choosenNumber ? '📈 too hight!' : '📉 too low!';
       Score--;
       document.querySelector('.score').textContent = Score;
     } else {
@@ -59,13 +45,40 @@ document.querySelector('.check').addEventListener('click', function () {
       );
       audiofail.play();
     }
+    // } else if (guess < choosenNumber) {
+    //   if (Score > 1) {
+    //     document.querySelector('.message').textContent = '📉 too low!';
+    //     Score--;
+    //     document.querySelector('.score').textContent = Score;
+    //   } else {
+    //     document.querySelector('.message').textContent =
+    //       '💥 you have lost the game';
+    //     document.querySelector('.score').textContent = 0;
+    //     const audioLoose = new Audio(
+    //       './Arcade game over sound effect_(MP3_320K).mp3'
+    //     );
+    //     audioLoose.play();
+    //   }
+    // } else if (guess > choosenNumber) {
+    //   if (Score > 1) {
+    //     document.querySelector('.message').textContent = '📈 too hight!';
+    //     Score--;
+    //     document.querySelector('.score').textContent = Score;
+    //   } else {
+    //     document.querySelector('.message').textContent =
+    //       '💥 you have lost the game';
+    //     document.querySelector('.score').textContent = 0;
+    //     const audiofail = new Audio(
+    //       './Arcade game over sound effect_(MP3_320K).mp3'
+    //     );
+    //     audiofail.play();
+    //   }
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   Score = 20;
   choosenNumber = Math.trunc(Math.random() * 20) + 1;
-  console.log(choosenNumber);
 
   document.querySelector('.score').textContent = Score;
   document.querySelector('.number').textContent = '?';
